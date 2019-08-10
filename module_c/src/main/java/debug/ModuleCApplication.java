4888c037ceab99e07b2201c2677bb22ae4433984
wuzuchang <Wzc949588>
2019-08-10 16:11:35 +0800

    加入WMRouter实现页面跳转

--- module_c/src/main/java/debug/ModuleCApplication.java
+++ module_c/src/main/java/debug/ModuleCApplication.java
@@ -2,10 +2,21 @@ package debug;
 
 import android.app.Application;
 
+import com.sankuai.waimai.router.Router;
+import com.sankuai.waimai.router.common.DefaultRootUriHandler;
+
 /**
  * Author: Wuzuchang
  * Date: 2019/8/4 18:48
  * Description: ${DESCRIPTION}
  */
 public class ModuleCApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // 创建RootHandler
+        DefaultRootUriHandler rootHandler = new DefaultRootUriHandler(this.getApplicationContext());
+        // 初始化
+        Router.init(rootHandler);
+    }
 }
